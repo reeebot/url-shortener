bc83c2026f12cbf753ddc55ca259c287c2ddf816
Tim Meijome <tim.meijome@gmail.com>
2016-04-15 21:05:39 +0000

    update

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express')
 var app = express()
-var moment = require('moment');
-var url = require('url');
 var port = process.env.PORT || 8080;
 
 app.route('/').get(function (req, res) {
@@ -11,22 +9,6 @@ app.route('/').get(function (req, res) {
 app.get('/*', function(req, res) {
     var requrl = url.parse(req.url).pathname;
     var cleanreq = requrl.replace(/^\/|\/$|%20+/g, '');
-
-    if (moment(+cleanreq).isValid()|moment(cleanreq).isValid()){
-        
-        if (isNaN(cleanreq)) {
-            var unixtime = moment(cleanreq.toString(), "MMMM-DD-YYYY").unix()
-            var naturaltime = moment(cleanreq.toString(), "MMMM-DD-YYYY").format("MMMM D, YYYY")
-        }
-        else {
-            var unixtime = moment.unix(+cleanreq, "MMMM-DD-YYYY").unix()
-            var naturaltime = moment.unix(+cleanreq).format("MMMM D, YYYY")
-        }
-    }
-    else {
-        var unixtime = null
-        var naturaltime = null
-    }
     
     var jsonstring = {
         unix : unixtime,
@@ -36,5 +18,5 @@ app.get('/*', function(req, res) {
 });
 
 app.listen(port, function() {
-	console.log('Node.js listening on port 8080 ...')
+	console.log('server listening')
 })
