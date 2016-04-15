9ae481da736ac9934eaa5da0cde7180fab61963f
Tim Meijome <tim.meijome@gmail.com>
2016-04-15 20:54:59 +0000

    update

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
-'use strict';
-
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
-var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
+var express = require('express')
+var app = express()
+var moment = require('moment');
+var url = require('url');
 var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+
+app.route('/').get(function (req, res) {
+	res.sendFile(process.cwd()+'/public/index.html');
 });
+
+app.get('/*', function(req, res) {
+    var requrl = url.parse(req.url).pathname;
+    var cleanreq = requrl.replace(/^\/|\/$|%20+/g, '');
+
+    if (moment(+cleanreq).isValid()|moment(cleanreq).isValid()){
+        
+        if (isNaN(cleanreq)) {
+            var unixtime = moment(cleanreq.toString(), "MMMM-DD-YYYY").unix()
+            var naturaltime = moment(cleanreq.toString(), "MMMM-DD-YYYY").format("MMMM D, YYYY")
+        }
+        else {
+            var unixtime = moment.unix(+cleanreq, "MMMM-DD-YYYY").unix()
+            var naturaltime = moment.unix(+cleanreq).format("MMMM D, YYYY")
+        }
+    }
+    else {
+        var unixtime = null
+        var naturaltime = null
+    }
+    
+    var jsonstring = {
+        unix : unixtime,
+        natural : naturaltime
+    }
+    res.send(JSON.stringify(jsonstring))
+});
+
+app.listen(port, function() {
+	console.log('Node.js listening on port 8080 ...')
+})
