ce677b164c9d7f96c5f28e4ae6d63478f460d6c6
Tim Meijome <tim.meijome@gmail.com>
2016-04-17 05:46:12 +0000

    update

--- mongo-query.js
+++ mongo-query.js
@@ -1,15 +1,16 @@
 var mongo = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/';
 
-module.exports = function Query(random, split){mongo.connect(url, function(err, db) {
-    if (err) throw err
-    var pull = db.collection('shorturl').find({
-        short_id : split[1]
-    });
-    console.log(pull)
-    pull.toArray(function(err, data) {
+module.exports = function Query(random, split){
+    mongo.connect(url, function(err, db) {
         if (err) throw err
-        console.log(data);
-        db.close();
+        var pull = db.collection('shorturl').find({
+            short_id : split[1]
+        });
+        pull.toArray(function(err, data) {
+            if (err) throw err
+            console.log(data);
+            db.close();
+        });
     });
-})};
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var url = require('url')
+var validator = require('validator');
 var port = process.env.PORT || 8080;
 var Input = require('./mongo-input')
 var Query = require('./mongo-query')
@@ -14,39 +15,39 @@ app.get('/*', function(req, res) {                      ////// compute the reque
     var cleanreq = requrl.replace(/^\//g, '');          	    // remove first forward slash/
     var split = cleanreq.split(/\/(.+)/, 2);	                // split into array of two after the first forward slash/ if present
 
-    if (split[0] === "new") {					        ////// request is new entry
-        if (split[1]){ //invalid URL format ie http://www
-        
-    }
-        else {
+    if (split[0] === "new") {					        ////// request is a new entry
+        if (validator.isURL(split[1])){                         //checks if valid URL format
         var random = Math.floor(Math.random()*9000) + 1000;     // create random 4 digit number
         var input = new Input(random, split);                   // input to database: random & split[1]  (assumes random number doesn't already exist in database, will overwrite previous entry if it exists)
 		var jsonresponse = {                                    // display original and short urls to user
             original_url : split[1],
             short_url : "http://rebot-fcc-third.herokuapp.com/" + random
             }
-        res.send(JSON.stringify(jsonresponse))
+        }
+        else {                                                  //invalid URL format
+            var jsonresponse = {
+                error : "Invalid URL Format."
+            }
         }
     }
-    
-    else if (Number.isInteger(+split[0])) {	        	////// request is existing entry
-    	//check database for request 
-        var query = new Query(random, split);
-    	//if request exists, redirect to stored url
+    else if (Number.isInteger(+split[0])) {	        	////// request is an existing entry
+        var query = new Query(random, split)                    //query database with request
         
-            //if request doesn't exist:
-            var jsonresponse = {
-                error : "This URL is not in the database."
-            }
-            res.send(JSON.stringify(jsonresponse))
-		res.send("redirecting...")
+        //if request exists, redirect to stored url
+        
+        //if request doesn't exist:
+            //var jsonresponse = {
+            //    error : "This URL is not in the database."
+            //}
+            
+		//res.send("redirecting...")
     }
-    else {									        	////// request doesn't exist
+    else {									        	////// invalid request
     	var jsonresponse = {
-    		error : "Invalid URL Format."
+    		error : "Invalid request. Please try again."
     	}
-    	res.send(JSON.stringify(jsonresponse))
     }
+    res.send(JSON.stringify(jsonresponse))
 });
 
 app.listen(port, function() {                           ////// start server
